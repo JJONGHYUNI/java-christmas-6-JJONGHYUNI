f892c57c9d93b03eb84d656d42e212c7086d1d6c
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-11-10 19:55:53 +0900

    feat(model) : MainMenu에서 주말 할인 메소드 상속받아 구현

--- src/main/java/christmas/model/menu/Drink.java
+++ src/main/java/christmas/model/menu/Drink.java
@@ -2,7 +2,7 @@ package christmas.model.menu;
 
 import christmas.model.menu.constant.MenuItem;
 
-public class Drink extends Menu{
+public class Drink extends Menu {
     public Drink(MenuItem menuItem, int count) {
         super(menuItem, count);
     }
--- src/main/java/christmas/model/menu/MainMenu.java
+++ src/main/java/christmas/model/menu/MainMenu.java
@@ -1,9 +1,16 @@
 package christmas.model.menu;
 
+import christmas.model.event.Event;
+import christmas.model.event.constant.EventConstants;
 import christmas.model.menu.constant.MenuItem;
 
-public class MainMenu extends Menu{
+public class MainMenu extends Menu implements Event {
     public MainMenu(MenuItem menuItem, int count) {
         super(menuItem, count);
+    }
+
+    @Override
+    public int salePrice() {
+        return getCount() * EventConstants.WEEKEND_EVENT_SALE_PRICE.getSalePrice();
     }
 }

bcd30a6fc647e21b65c0470d20de78f457963775
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-11-11 18:15:53 +0900

    test(model) : 존재하지 않는 메뉴 입력 시 예외 발생 테스트

--- src/test/java/christmas/model/order/OrderTest.java
+++ src/test/java/christmas/model/order/OrderTest.java
@@ -18,4 +18,15 @@ class OrderTest {
         assertThatThrownBy(() -> new Order(orderInput))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("주문 입력 메뉴가 존재하지 않는 메뉴일 시 예외가 발생한다")
+    @Test
+    void createOrderNotContainMenuItem() {
+        //given
+        String orderInput = "종현이-1";
+
+        //when,then
+        assertThatThrownBy(() -> new Order(orderInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

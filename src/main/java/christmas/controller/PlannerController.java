c91936887009888568299df10353c610c1661e66
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-11-13 20:42:58 +0900

    feat(controller) : event와 메뉴를 입력 받아 Order 생성하도록 구현

--- src/main/java/christmas/controller/PlannerController.java
+++ src/main/java/christmas/controller/PlannerController.java
@@ -2,17 +2,21 @@ package christmas.controller;
 
 import christmas.model.day.Day;
 import christmas.model.order.Order;
+import christmas.service.EventService;
 import christmas.view.InputView;
 
 public class PlannerController {
     private final InputView inputView;
+    private final EventService eventService;
 
     public PlannerController() {
         inputView = new InputView();
+        eventService = new EventService();
     }
 
     public void planning() {
         Day day = new Day(getDate());
+        Order order = new Order(getOrder(), eventService.createEvent(day));
     }
 
     private String getDate() {

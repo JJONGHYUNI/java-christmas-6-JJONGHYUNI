415672d77b3db47622e6a7386da08a92b15538ba
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-11-13 20:50:45 +0900

    feat(view) : outputview에서 이벤트 혜택 미리 보기 메시지 출력 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,7 +3,11 @@ package christmas.view;
 import christmas.view.constant.OutputMessageConstants;
 
 public class OutputView {
-    public static void printStratMessage() {
+    public void printStartMessage() {
         System.out.println(OutputMessageConstants.START_MESSAGE.getMessage());
+    }
+
+    public void printPreviewEventMessage() {
+        System.out.println(OutputMessageConstants.PREVIEW_EVENT_MESSAGE.getMessage());
     }
 }

994023a31c21e7a5afef0dccf441b6e447dcd3f2
JJONGHYUNI <whdgusdl8792@gmail.com>
2023-11-15 02:48:10 +0900

    feat(model) : 기프트 이벤트에서 증정 상품 구하는 메소드 구현

--- src/main/java/christmas/model/event/GiftEvent.java
+++ src/main/java/christmas/model/event/GiftEvent.java
@@ -3,18 +3,28 @@ package christmas.model.event;
 import christmas.model.event.constant.EventConstants;
 import christmas.model.event.constant.EventRule;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class GiftEvent {
-    public Map<String, Integer> receiveGift(int totalPrice) {
-        if(isGiftEventApplied(totalPrice)) {
-            return Map.of(EventConstants.GIFT_EVENT_PRICE.getSaleTitle(), EventConstants.GIFT_EVENT_PRICE.getSalePrice());
-        };
-        return Map.of(EventConstants.DISCOUNT_NOT_APLLIED.getSaleTitle(), EventConstants.DISCOUNT_NOT_APLLIED.getSalePrice());
+    public Map<String, Integer> receiveGiftItem(int totalPrice) {
+        if (isGiftEventApplied(totalPrice)) {
+            return Map.of(EventConstants.GIFT_EVENT_ITEM.getSaleTitle(), EventConstants.GIFT_EVENT_ITEM.getSalePrice());
+        }
+        return receiveNotAppliedEvent();
     }
 
-    private boolean isGiftEventApplied(int totalPrice) {
+    public Map<String, Integer> receiveGiftPrice(int totalPrice) {
+        if(isGiftEventApplied(totalPrice)) {
+            return Map.of(EventConstants.GIFT_EVENT_PRICE.getSaleTitle(), EventConstants.GIFT_EVENT_PRICE.getSalePrice());
+        }
+        return receiveNotAppliedEvent();
+    }
+
+    private static boolean isGiftEventApplied(int totalPrice) {
         return totalPrice >= EventRule.GIFT_EVENT_APPLY_AMOUNT.getAmount();
+    }
+
+    private static Map<String,Integer> receiveNotAppliedEvent() {
+        return Map.of(EventConstants.DISCOUNT_NOT_APLLIED.getSaleTitle(), EventConstants.DISCOUNT_NOT_APLLIED.getSalePrice());
     }
 }
--- src/main/java/christmas/model/event/constant/EventConstants.java
+++ src/main/java/christmas/model/event/constant/EventConstants.java
@@ -7,6 +7,7 @@ public enum EventConstants {
     WEEKEND_EVENT_SALE_PRICE("주말 할인 : ", -2023),
     SPECIAL_EVENT_SALE_PRICE("특별 할인 : ", -1000),
     GIFT_EVENT_PRICE("증정 할인 : ", -25000),
+    GIFT_EVENT_ITEM("샴페인", 1),
     DISCOUNT_NOT_APLLIED("없음", 0);
 
     private final String saleTitle;
